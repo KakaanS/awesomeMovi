ff4c0acc4762dbd6a12616005775a2696ffa9f7f
Oscar Wendt <oscar.wendt91@gmail.com>
2023-09-21 14:37:55 +0200

    added componentLogin+ctxFile+mockServer

---
+++ src/component/Login.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const Login = () => {
+  const [userName, setUsername] = useState("");
+  const [passWord, setPassword] = useState("");
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={loginHandler}>
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/PageLogin.jsx
@@ -0,0 +1,11 @@
+import Login from "../component/Login";
+
+const PageLogin = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default PageLogin;
