8bcc0c1cb57e44c010532e16881ec65adc861dd5
Oscar Wendt <oscar.wendt91@gmail.com>
2023-09-21 14:37:55 +0200

    added componentLogin+ctxFile+mockServer

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Home from './pages/home.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Home from "./pages/home.jsx";
 
-import './index.css'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Home />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
