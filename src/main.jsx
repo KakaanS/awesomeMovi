4f279be9962ecf511cf9ba5575b14454a644d684
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-09-22 11:11:18 +0200

    removed unsused code, moved Home into App

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Home from './pages/home.jsx'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+   <App />
   </React.StrictMode>,
 )
