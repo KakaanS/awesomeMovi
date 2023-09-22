5cca383b3036702d93f67ec8dd9f3e15eb6205ee
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-09-22 13:21:42 +0200

    formatted

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home.jsx";
 
@@ -6,13 +11,11 @@ import Home from "./pages/home.jsx";
  *
  * @returns Lägg till conditinal rendering här för att visa Home när man loggat in eller Login när användaren inte är autentisierad. Tex element= {token ? <Home /> : <Navigate to='/login'}
  */
-const router = createBrowserRouter(createRoutesFromElements(
-   <Route path='/' element= {<Home />}/>
-))
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Home />} />)
+);
 function App() {
-  return (
-   <RouterProvider router={router} />
-   );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
