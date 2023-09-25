07fe94e021faee19f65739695589b3d16865742f
Oscar Wendt <oscar.wendt91@gmail.com>
2023-09-25 10:40:13 +0200

    added function and context for login

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,6 +1,38 @@
+import { useState } from "react";
+
+// function & styling
+import { useAuth } from "../context/AuthCtx";
+
+/**
+ * Login component, with loginHandler function making the API call to mockServer
+ */
+
 const Login = () => {
-  const loginHandler = (e) => {
+  const { login } = useAuth();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginHandler = async (e) => {
     e.preventDefault();
+    try {
+      const resp = await fetch("mockServerApiURL", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      if (resp.ok) {
+        const data = await resp.json();
+        console.log(data);
+        const token = data.token;
+        login(token);
+      } else {
+        console.log("wrong crendentials");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
