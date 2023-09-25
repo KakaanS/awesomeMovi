4712a5937f586d864c05c1c65854b845be9fbd92
stephanie <stephanie.fadel@hotmail.com>
2023-09-25 17:28:28 +0200

    Add: Components added to homepage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,17 @@
-import RecomendedMovies from "../component/RecomendedMovies"
+import React from 'react';
+import RecomendedMovies from "../component/RecomendedMovies";
+import Navbar from "../component/ui/Navbar";
+import Title from "../component/ui/Title"; 
+
 
 const Home = () => {
     return (
         <div>
-        < RecomendedMovies />
+            <Navbar />
+            <RecomendedMovies />
+            <Title />
         </div>
     )
 }
 
-export default Home
+export default Home;
