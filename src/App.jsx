977b5daa399b60d2d3f43d5a8f18ab465c18481f
Oscar Wendt <oscar.wendt91@gmail.com>
2023-09-25 14:05:45 +0200

    msw working, login working

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/home.jsx";
-import Login from "./component/Login";
+import PageLogin from "./pages/Login.jsx";
 
 /**
  *
  * @returns Lägg till conditinal rendering här för att visa Home när man loggat in eller Login när användaren inte är autentisierad. Tex element= {token ? <Home /> : <Navigate to='/login'}
  */
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Login />} />)
+  createRoutesFromElements(<Route path="/" element={<PageLogin />} />)
 );
 function App() {
   return <RouterProvider router={router} />;
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,15 @@ import App from "./App.jsx";
 import "./index.css";
 import AllCtx from "./context/allCtx.jsx";
 
+// We seriously need to setup ESLINT.config and manage rules
+
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV === "development") {
+  import("./server/browser.js").then(({ worker }) => {
+    worker.start();
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AllCtx>
