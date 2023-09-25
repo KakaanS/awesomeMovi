9ca22b34453929fa8598797f9f35c1861763b95d
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-09-25 12:06:06 +0200

    unique genres filtered

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -2,7 +2,14 @@ import { useState } from "react";
 import movieData from "../data/movies.json"
 
 const Categories = () => {
-   const [categories, setCategories] = useState([])
+   const [categories, setCategories] = useState([]);
+
+ const uniqueGenres =[ 
+        ...new Set (movieData
+          .flatMap((movie) => movie.genre.split(", "))
+          .filter((genre) => genre))]
+
+    console.log(uniqueGenres)
 
     return (
         <div>
