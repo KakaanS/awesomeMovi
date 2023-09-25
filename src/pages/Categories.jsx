d66a537bd2b393be6d1e331c2e62545176897471
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-09-25 11:42:51 +0200

    categories page created

---
+++ src/pages/Categories.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import movieData from "../data/movies.json"
+
+const Categories = () => {
+   const [categories, setCategories] = useState([])
+
+    return (
+        <div>
+            Hello world
+        </div>
+    )
+}
+
+export default Categories
