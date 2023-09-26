bc289f16d73bcee9b1a799e011f904f710d726cf
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-09-26 18:55:08 +0200

    Route testing for landing page

--- src/test/App.test.jsx
+++ src/test/App.test.jsx
@@ -1,8 +1,31 @@
-import { expect, test } from "vitest"
-import { render } from "@testing-library/react"
-import App from "../App"
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
 
 test("should always pass", () => {
-   render(<App />)
-   expect(true).toBe(true)
-})
+  render(<App />, { wrapper: BrowserRouter });
+  expect(true).toBe(true);
+});
+
+// Test to verify that the landing-page (home) is fully rendered
+test("full app rendering landing page", async () => {
+  render(<App />, { wrapper: BrowserRouter });
+
+  expect(screen.getByText("HOME")).toBeInTheDocument();
+  expect(screen.getByText("CATEGORY")).toBeInTheDocument();
+  expect(screen.getByText("BOOKMARKS")).toBeInTheDocument();
+  expect(screen.getByText("Recommended for you")).toBeInTheDocument();
+  expect(screen.getByText("Trending")).toBeInTheDocument();
+});
+
+// Test to verify that the content is not rendered on a bad route
+test("landing on a bad page", () => {
+  const badRoute = "/some/bad/route";
+  render(
+    <MemoryRouter initialEntries={[badRoute]}>
+      <App />
+    </MemoryRouter>
+  );
+  expect(screen.queryByText("HOME")).toBeNull();
+});
