440e6c4e63905c8c3f0ca13d27ea85a6ceb6c8bb
0m4r123 <114648195+0m4r123@users.noreply.github.com>
2023-09-27 15:13:11 +0200

    Imported navbar into MovieDetail Component

--- src/component/MovieDetail.jsx
+++ src/component/MovieDetail.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-
+import Navbar from "../component/ui/Navbar";
 const MovieDetail = ({ movie }) => {
   return (
     <div>
+      <Navbar />
+      <SearchBox />
       <h2>{movie.title}</h2>
       <p>RATING: {movie.rating}</p>
       <p>ACTORS: {movie.actors}</p>
