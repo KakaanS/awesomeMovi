3c8f15301710bc6914c918a463da75f9de86196b
0m4r123 <114648195+0m4r123@users.noreply.github.com>
2023-09-27 15:05:13 +0200

    Added route for MovieDetailPage, imported Link from route-dom and parssed the movie id in the url in TrendingMovies. The image and title is now clickable in TrendningMovies, using link. Passed the movie id from TrendingMovies  to MovieDetail and returned the details for the movie.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home.jsx";
 import Categories from "./pages/Categories";
-
+import MovieDetailPage from "./pages/MovieDetail";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/categories" element={<Categories />} />
+      <Route path="/movie/:id" element={<MovieDetailPage />} />
     </Routes>
   );
 }
--- src/component/TrendingMovies.jsx
+++ src/component/TrendingMovies.jsx
@@ -1,30 +1,31 @@
-import dataBase from '../data/movies.json'
+import dataBase from "../data/movies.json";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 
 const TrendingMovies = () => {
-
   // Picks out the movies that have trending = true in the database
+  function getTrendningMovies(dataBase) {
+    return dataBase.filter((movie) => movie.isTrending === true);
+  }
 
-    function getTrendningMovies(dataBase) {
-        return dataBase.filter(movie => movie.isTrending === true)
-    }
-
-    const trendingMovies = getTrendningMovies(dataBase)
+  const trendingMovies = getTrendningMovies(dataBase);
 
   // we return the list of the trending movies
 
-    return (
-        <div>
-        <h2>Trending</h2>
-        <ul>
-          {trendingMovies.map(movie => (
-            <li key={movie.id}>
-                <img  src={movie.thumbnail} alt={movie.name}/>
+  return (
+    <div>
+      <h2>Trending</h2>
+      <ul>
+        {trendingMovies.map((movie) => (
+          <li key={movie.id}>
+            <Link to={`/movie/${movie.id}`}> {/* Makes thumbnail and title pressable and passes the id in the url */}
+              <img src={movie.thumbnail} alt={movie.name} /> 
               <p>{movie.title}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
-    )
-}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default TrendingMovies
+export default TrendingMovies;
