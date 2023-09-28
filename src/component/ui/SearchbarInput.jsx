07b60aafa4e0d3fe90293d38d044cd09184d3f9a
stephanie <stephanie.fadel@hotmail.com>
2023-09-28 13:30:57 +0200

    Changed: align the searchbar directly in component

--- src/component/ui/SearchbarInput.jsx
+++ src/component/ui/SearchbarInput.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
 
 const SearchbarInput = ({ value, onChange }) => {
-    const SearchbarStyle = {
-        padding: '15px',
-        textAlign: 'left',
-        margin: '40px',
-        backgroundColor: 'white',
-        width: '40%',
-        borderRadius: '50px',
-        outline: 'none',
-        border: 'none',
+  const SearchbarContainerStyle = {
+    display: 'flex',
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    height: '100px', 
+  };
 
- };
+  const SearchbarStyle = {
+    padding: '15px',
+    textAlign: 'left',
+    backgroundColor: 'white',
+    width: '40%',
+    borderRadius: '50px',
+    outline: 'none',
+    border: 'none',
+  };
 
-    return (
-        <input
-            style={SearchbarStyle}
-            type="text"
-            placeholder="Search for a movie..."
-            value={value}
-            onChange={onChange}
-        />
-    );
+  return (
+    <div style={SearchbarContainerStyle}>
+      <input
+        style={SearchbarStyle}
+        type="text"
+        placeholder="Search for a movie..."
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
 };
 
 export default SearchbarInput;
