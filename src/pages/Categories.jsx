f49c67a0f9ee5a1792c2a819f33a86edc6a8cf5a
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-09-29 14:11:00 +0200

    Added All movies button and clickable categories
    Categories are clickable and shows movies from selected category. Default is show all movies.

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
 import movieData from "../data/movies.json";
 import Category from "../component/Category";
-import Navbar from "../component/ui/Navbar";
-import Title from "../component/ui/Title";
+import MovieCard from "../component/ui/MovieCard";
 
 /**
- *
- * @returns All categories including all the movies with the category
+ * Renders Alist of categories and movies. Allows user to select a category to view its movies
+ * 
  */
 
-const Categories = () => {
 
+const Categories = () => {
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [showAllMovies, setShowAllMovies] = useState(true);
 
   // Filter the genres to extract one of each
   useEffect(() => {
@@ -25,28 +25,38 @@ const Categories = () => {
     ];
     setCategories(uniqueGenres);
   }, []);
+
   // Toggles the movies in category when clicked
   const handleCategoryClick = (category) => {
-    setSelectedCategory(selectedCategory === category ? null : category);
+    setSelectedCategory(category);
+    setShowAllMovies(false);
   };
+  // Shows all movies from the data
+  const handleShowAllMovies = () => {
+   setShowAllMovies(true)
+   setSelectedCategory(null)
+  }
 
   return (
     <div>
       <h2>Categories</h2>
+      <button onClick={handleShowAllMovies}>All movies</button>
+      <ul>
       {categories.map((category, index) => (
-        <div key={index}>
-          <div  onClick={() => handleCategoryClick(category)}>
-            {category}
-          </div>
-          {selectedCategory === category && (
-            <Category key={index} category={category}/>
-          )}
-        </div>
+        <li key={index}>
+          <button onClick={() => handleCategoryClick(category)}>{category}</button>
+        </li>
       ))}
+      </ul>
+      {selectedCategory && <Category category={selectedCategory} />}
+      {showAllMovies &&
+        movieData.map((movie) => (
+          <div key={movie.id}>
+            <MovieCard movie={movie} />
+          </div>
+        ))}
     </div>
   );
 };
 
 export default Categories;
-
-// 
