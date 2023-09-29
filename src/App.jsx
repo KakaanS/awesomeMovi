f587162113891018008dfd2a26e0ce0014c35659
0m4r123 <114648195+0m4r123@users.noreply.github.com>
2023-09-29 13:57:54 +0200

    Committing changes to src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/home.jsx";
+import Home from "./pages/Home";
 import Categories from "./pages/Categories";
 import PageLogin from "./pages/Login";
 import MovieDetailPage from "./pages/MovieDetail";
 function App() {
-
   return (
     <Routes>
       <Route path="/awesomeMovi/login" element={<PageLogin />} />
