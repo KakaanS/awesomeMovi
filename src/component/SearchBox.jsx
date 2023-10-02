4de060e0e8319ae9189a359fb79be1125682299f
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-10-02 15:56:58 +0200

    Remove trending and recommended when searched movie exists

--- src/component/SearchBox.jsx
+++ src/component/SearchBox.jsx
@@ -1,14 +1,10 @@
-// SearchBox.jsx
 import { useState, useEffect } from "react";
 import movieData from "../data/movies.json";
-import MovieCard from "./ui/MovieCard";
 import SearchbarInput from "./ui/SearchbarInput";
-import { Link } from "react-router-dom"; 
 
 
-const SearchBox = () => {
+const SearchBox = ({setSearchResults}) => {
   const [searchText, setSearchText] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
 
   // filters the list of movies depending on letters / names
 
@@ -17,11 +13,11 @@ const SearchBox = () => {
       setSearchResults([]);
     } else {
       const filteredMovies = movieData.filter((movie) =>
-        movie.title.toLowerCase().startsWith(searchText.toLowerCase())
+        movie.title.toLowerCase().startsWith(searchText.toLowerCase()) || movie.title.toLowerCase().includes(searchText.toLowerCase())
       );
       setSearchResults(filteredMovies);
     }
-  }, [searchText]);
+  }, [searchText, setSearchResults]);
 
   //returns list of the movies
 
@@ -34,15 +30,6 @@ const SearchBox = () => {
         type="search"
       />
 
-      <div>
-        <div>
-          {searchResults.map((movie) => (
-            <Link key={movie.id} to={`/movie/${movie.id}`}> {/* Makes thumbnail and title pressable and passes the id in the url */}
-              <MovieCard key={movie.id} movie={movie} />
-            </Link>
-          ))}
-        </div>
-      </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import RecomendedMovies from "../component/RecomendedMovies";
 import TrendingMovies from "../component/TrendingMovies";
 import SearchBox from "../component/SearchBox";
 import Navbar from "../component/ui/Navbar";
+import MovieCard from "../component/ui/MovieCard";
 
 const Home = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
   return (
     <div>
       <Navbar />
-      <SearchBox />
-      <RecomendedMovies />
-      <TrendingMovies />
+      <SearchBox setSearchResults={setSearchResults} />
+      {searchResults.length > 0 ? (
+        <div>
+          {searchResults.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <RecomendedMovies />
+          <TrendingMovies />
+        </div>
+      )}
     </div>
   );
 };
