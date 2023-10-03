18bb0842f16b9e8eb2172807be42265e205ae984
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-10-03 15:46:03 +0200

    Moved state up one level for access

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,20 +2,19 @@ import { useState } from "react";
 import RecomendedMovies from "../component/RecomendedMovies";
 import TrendingMovies from "../component/TrendingMovies";
 import SearchBox from "../component/SearchBox";
-import Navbar from "../component/ui/Navbar";
 import MovieCard from "../component/ui/MovieCard";
 
 const Home = () => {
   const [searchResults, setSearchResults] = useState([]);
-    const [searchText, setSearchText] = useState("");
+  const [searchText, setSearchText] = useState("");
 
 
   return (
     <div>
-      <Navbar />
       <SearchBox setSearchResults={setSearchResults} searchText={searchText} setSearchText={setSearchText} />
       {searchResults.length > 0 || searchText ? (
         <div>
+         <p>Your searchword: {searchText}</p>
           {searchResults.map((movie) => (
             <MovieCard key={movie.id} movie={movie} />
           ))}
