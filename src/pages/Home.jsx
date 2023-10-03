4be18c3b51d26b9195e5dabbc069c47cb1420fea
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-10-03 16:54:50 +0200

    Message displayed when theres no search result

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,29 @@ import RecomendedMovies from "../component/RecomendedMovies";
 import TrendingMovies from "../component/TrendingMovies";
 import SearchBox from "../component/SearchBox";
 import MovieCard from "../component/ui/MovieCard";
+import Navbar from "../component/ui/Navbar";
 
 const Home = () => {
   const [searchResults, setSearchResults] = useState([]);
   const [searchText, setSearchText] = useState("");
 
-
   return (
     <div>
-      <SearchBox setSearchResults={setSearchResults} searchText={searchText} setSearchText={setSearchText} />
+      <Navbar />
+      <SearchBox
+        setSearchResults={setSearchResults}
+        searchText={searchText}
+        setSearchText={setSearchText}
+      />
+
+      {/* Display message when there are no search results */}
+      {searchResults.length === 0 && searchText ? (
+        <p>Your search for &quot;{searchText}&quot; did not have any matches</p>
+      ) : null}
+
+      {/* Display search results or default content */}
       {searchResults.length > 0 || searchText ? (
         <div>
-         <p>Your searchword: {searchText}</p>
           {searchResults.map((movie) => (
             <MovieCard key={movie.id} movie={movie} />
           ))}
