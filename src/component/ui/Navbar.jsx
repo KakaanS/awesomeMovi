f56704cb13b139fb9a5f3f8ea910986879536233
0m4r123 <114648195+0m4r123@users.noreply.github.com>
2023-10-04 19:30:49 +0200

    Phone navbar design and minor changes in Navbar component

--- src/component/ui/Navbar.jsx
+++ src/component/ui/Navbar.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "./Button";
 import { useAuth } from "../../context/AuthCtx";
@@ -5,6 +6,7 @@ import "../../mobilecss/navbar.css";
 
 const Navbar = () => {
   const { logout } = useAuth();
+  const [phoneMenuVisible, setPhoneMenuVisible] = useState(false);
 
   const handleHomeClick = () => {
     if (location.pathname === "/") {
@@ -18,6 +20,9 @@ const Navbar = () => {
     window.location.reload();
   };
 
+  const togglePhoneMenu = () => {
+    setPhoneMenuVisible(!phoneMenuVisible);
+  };
   return (
     <nav className="navbarStyle">
       <ul className="ulStyle">
@@ -40,6 +45,37 @@ const Navbar = () => {
           <Button text="Log Out" onClick={handleLogout} />
         </li>
       </ul>
+      {/* Phone menu button (visible on phones) */}
+      <div className="phoneMenuButton" onClick={togglePhoneMenu}>
+        {/* Mobile menu button with three horizontal lines */}
+        <div className="menuBar"></div>
+        <div className="menuBar"></div>
+        <div className="menuBar"></div>
+      </div>
+
+      {/* Phone menu (visible on phones when the button is clicked) */}
+      {phoneMenuVisible && (
+        <ul className="phoneMenu">
+          <li className="phoneMenuItem">
+            <Link to="/" className="linkStyle" onClick={handleHomeClick}>
+              HOME
+            </Link>
+          </li>
+          <li className="phoneMenuItem">
+            <Link to="/categories" className="linkStyle">
+              CATEGORY
+            </Link>
+          </li>
+          <li className="phoneMenuItem">
+            <Link to="/bookmark" className="linkStyle">
+              BOOKMARKS
+            </Link>
+          </li>
+          <li className="phoneMenuItem">
+            <Button text="Log Out" onClick={handleLogout} />
+          </li>
+        </ul>
+      )}
     </nav>
   );
 };
