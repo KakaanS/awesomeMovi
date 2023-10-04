6f041eda1d97bbe67730e110ee346749b1c59e81
stephanie <stephanie.fadel@hotmail.com>
2023-10-04 15:16:45 +0200

    Move: into new css

--- src/component/ui/Form.jsx
+++ src/component/ui/Form.jsx
@@ -1,25 +1,13 @@
+import "../../mobilecss/form.css";
+
 const Form = ({ children, onSubmit }) => {
-    const formContainerStyle = {
-        display: 'flex',
-        flexDirection: 'column', 
-        alignItems: 'center',
-        width: '100%',
-        justifyContent: 'center',
-    };
-
-    const formStyle = {
-        width: '60%',
-        display: 'flex',
-        flexDirection: 'column',
-    };
-
-    return (
-        <div style={formContainerStyle}>
-            <form onSubmit={onSubmit} style={formStyle}>
-                {children}
-            </form>
-        </div>
-    );
+  return (
+    <div className="formContainerStyle">
+      <form onSubmit={onSubmit} className="formStyle">
+        {children}
+      </form>
+    </div>
+  );
 };
 
 export default Form;
