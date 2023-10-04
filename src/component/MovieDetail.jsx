2b4c65d2343a543ffd632478ced35efabf118ddf
stephanie <stephanie.fadel@hotmail.com>
2023-10-04 17:20:14 +0200

    Add: new styling css

--- src/component/MovieDetail.jsx
+++ src/component/MovieDetail.jsx
@@ -1,22 +1,25 @@
 import Navbar from "../component/ui/Navbar";
-import TwoColumnLayout from './ui/TwoColumnLayout';
+import TwoColumnLayout from "./ui/TwoColumnLayout";
 import TitleLine from "./ui/TitleLine";
 import TextParagraph from "./ui/TextParagraph";
-import BookMark from "./BookMarkButton"
+import BookMark from "./BookMarkButton";
+import "../mobilecss/movie-detail.css"
 
 const MovieDetail = ({ movie }) => {
   return (
-    <div>
+    <div className="movie-detail-container">
       <Navbar />
       <TitleLine text={movie.title} />
       <TwoColumnLayout imageSrc={movie.thumbnail} imageAlt={movie.name}>
-        <div>
-          <TextParagraph>RATING:</TextParagraph>{movie.rating}
-          <TextParagraph>ACTORS: </TextParagraph>{movie.actors}
-          <TextParagraph>GENRE: </TextParagraph>{movie.genre}
-          <TextParagraph>SYNOPSIS: </TextParagraph>{movie.synopsis}
+        <BookMark movie={movie} />
+        <div className="movie-detail-text">
+          <TextParagraph>RATING:</TextParagraph> {movie.rating}
+          <TextParagraph>ACTORS: </TextParagraph> {movie.actors}
+          <TextParagraph>GENRE: </TextParagraph>
+          {movie.genre}
+          <TextParagraph>SYNOPSIS: </TextParagraph>
+          {movie.synopsis}
         </div>
-        <BookMark movie={movie}/>
       </TwoColumnLayout>
     </div>
   );
--- src/component/ui/Button.jsx
+++ src/component/ui/Button.jsx
@@ -1,15 +1,11 @@
-const Button = ({ text, onClick, style }) => {
-  const buttonStyle = {
-    backgroundColor: '#008E7E',
-    borderRadius: '20px',
-    padding: '8px 20px',
-    color: '#fff',
-    cursor: 'pointer',
-    textTransform: 'uppercase',
-    ...style, 
-  };
+import "../../mobilecss/button.css";
 
-  return <button style={buttonStyle} onClick={onClick}>{text}</button>;
+const Button = ({ text, onClick }) => {
+  return (
+    <button className="buttonStyle" onClick={onClick}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
