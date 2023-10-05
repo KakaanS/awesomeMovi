923b309d2d63a3d18a7e7579afc25e31341c63d1
0m4r123 <114648195+0m4r123@users.noreply.github.com>
2023-10-05 18:21:40 +0200

    Responsive category filter

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -6,6 +6,8 @@ import Title from "../components/ui/Title";
 import Button from "../components/ui/Button";
 import Navbar from "../components/ui/Navbar";
 import ButtonFilter from "../components/ui/ButtonFilter";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
 /**
  * Renders Alist of categories and movies. Allows user to select a category to view its movies
  *
@@ -14,6 +16,7 @@ const CategoriesPage = () => {
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [showAllMovies, setShowAllMovies] = useState(true);
+  const [filterContainerVisible, setFilterContainerVisible] = useState(false); // Add this line to initialize filterContainerVisible
 
   // Filter the genres to extract one of each
   useEffect(() => {
@@ -38,33 +41,51 @@ const CategoriesPage = () => {
     setSelectedCategory(null);
   };
 
+  const toggleFilterContainer = () => {
+    setFilterContainerVisible(!filterContainerVisible);
+  };
+
   return (
     <div>
       <Navbar />
       <Title text="Categories" />
-      <Button
-        onClick={handleShowAllMovies}
-        text="All movies"
-        style={{ margin: "20px" }}
-      />
-      <ul style={{ display: "flex", flexWrap: "wrap", justifyContent: "left" }}>
-        {/* Display category buttons */}
-        {categories.map((category, index) => (
-          <div key={index} style={{ flexBasis: "10%", margin: "5px" }}>
-            <ButtonFilter
-              onClick={() => handleCategoryClick(category)}
-              text={category}
-              style={{ width: "100%" }}
-            />
-          </div>
-        ))}
-      </ul>
+      <div className="filter-btn" onClick={toggleFilterContainer}>
+        {/*         <FontAwesomeIcon icon={faFilter} />{" "}
+         */}{" "}
+        <Title text="Sort movies by genre: All Movies" />
+      </div>
+      {filterContainerVisible && (
+        <div className="all-filter-container">
+          <Button
+            onClick={handleShowAllMovies}
+            text="All movies"
+            style={{ margin: "20px" }}
+          />
+
+          <ul
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "left",
+            }}
+          >
+            {/* Display category buttons */}
+            {categories.map((category, index) => (
+              <div key={index} style={{ flexBasis: "10%", margin: "5px" }}>
+                <ButtonFilter
+                  onClick={() => handleCategoryClick(category)}
+                  text={category}
+                  style={{ width: "100%" }}
+                />
+              </div>
+            ))}
+          </ul>
+        </div>
+      )}
       {/* Display movies when a category is selected */}
       {selectedCategory && <Category category={selectedCategory} />}
       {showAllMovies &&
-        movieData.map((movie) => (
-            <MovieCard movie={movie} key={movie.id} />
-        ))}
+        movieData.map((movie) => <MovieCard movie={movie} key={movie.id} />)}
     </div>
   );
 };
