d2e4286d5af039f1f2de11be5e0f24d8591408fe
Natalie <natalie.hallerdal@yahoo.se>
2023-10-05 14:34:57 +0200

    Carousel added

--- src/components/RecomendedMovies.jsx
+++ src/components/RecomendedMovies.jsx
@@ -1,10 +1,12 @@
 import dataBase from '../data/movies.json';
-import MovieCard from './ui/MovieCard';
 import TitleLine from './ui/TitleLine';
+import MovieCard from './ui/MovieCard';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 const RecomendedMovies = () => {
   // Plocka ut filmer som inte har trending=true
-  const nonTrendingMovies = dataBase.filter(movie => movie.isTrending !== true);
+  const nonTrendingMovies = dataBase.filter((movie) => movie.isTrending !== true);
 
   // Blanda de icke-trending filmerna slumpmässigt
   function shuffleArray(array) {
@@ -17,15 +19,48 @@ const RecomendedMovies = () => {
   }
 
   // Välj fem slumpmässiga icke-trending filmer
-  const randomMovies = shuffleArray(nonTrendingMovies).slice(0, 5);
+  const randomMovies = shuffleArray(nonTrendingMovies).slice(0, 7);
+
+  // Organize randomMovies into groups of five
+  const groupedMovies = [];
+  for (let i = 0; i < randomMovies.length; i += 5) {
+    groupedMovies.push(randomMovies.slice(i, i + 5));
+  }
+
+  // Configure the carousel to show groups of five movies at a time
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
     <div>
       <TitleLine text="Recommended for you" />
-      <div>
-        {randomMovies.map((movie) => (
-            <MovieCard key={movie.id} movie={movie} />
+      <Carousel responsive={responsive}>
+        {groupedMovies.map((group, index) => (
+          <div key={index}>
+            <div className="group-of-movies">
+              {group.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </div>
+          </div>
         ))}
-      </div>
+      </Carousel>
     </div>
   );
 };
--- src/components/TrendingMovies.jsx
+++ src/components/TrendingMovies.jsx
@@ -1,7 +1,8 @@
-import dataBase from '../data/movies.json'
+import dataBase from '../data/movies.json';
 import TitleLine from './ui/TitleLine';
 import MovieCard from './ui/MovieCard';
-
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 const TrendingMovies = () => {
   // Picks out the movies that have trending = true in the database
@@ -10,17 +11,51 @@ const TrendingMovies = () => {
   }
 
   const trendingMovies = getTrendningMovies(dataBase);
-  // we return the list of the trending movies
+
+  // Organize trendingMovies into groups of five
+  const groupedMovies = [];
+  for (let i = 0; i < trendingMovies.length; i += 5) {
+    groupedMovies.push(trendingMovies.slice(i, i + 5));
+  }
+
+  // Configure the carousel to show groups of five movies at a time
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
     <div>
       <TitleLine text="Trending" />
-      <div>
-        {trendingMovies.map(movie => (
-            <MovieCard key={movie.id} movie={movie} />
+      <Carousel responsive={responsive}>
+        {groupedMovies.map((group, index) => (
+          <div key={index}>
+            <div className="group-of-movies">
+              {group.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </div>
+          </div>
         ))}
-      </div>
+      </Carousel>
     </div>
   );
 };
 
 export default TrendingMovies;
+
+
