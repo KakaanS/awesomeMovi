a19e73d6c8f31c160568c3d0bfbf7623612d99d6
Oscar Wendt <oscar.wendt91@gmail.com>
2023-10-05 09:48:01 +0200

    updated names and imports

--- src/pages/Home.test.jsx
+++ src/pages/Home.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { test, expect } from "vitest";
 import { MemoryRouter } from "react-router-dom";
-import userEvent  from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 import Home from "../pages/Home";
 
 // Test to verify that the landing-page (home) is fully rendered
@@ -13,28 +13,29 @@ test("full app rendering landing page", async () => {
   );
 
   expect(screen.getByText("HOME")).toBeInTheDocument();
-  screen.debug();
   expect(screen.getByText("CATEGORY")).toBeInTheDocument();
   expect(screen.getByText("BOOKMARKS")).toBeInTheDocument();
   expect(screen.getByText("Recommended for you")).toBeInTheDocument();
   expect(screen.getByText("Trending")).toBeInTheDocument();
 });
 
-   // test to see if it is possible to search for all movies starting with "t"
-   
-   test('searching for all movies that starts whit the letter "t"', async () => {
-     const { getByPlaceholderText, getAllByText } = render( <MemoryRouter>
+// test to see if it is possible to search for all movies starting with "t"
+
+test('searching for all movies that starts whit the letter "t"', async () => {
+  const { getByPlaceholderText, getAllByText } = render(
+    <MemoryRouter>
       <Home />
-    </MemoryRouter>);
+    </MemoryRouter>
+  );
 
-     const inputElement = getByPlaceholderText("Search for a movie...");
-   
-     userEvent.type(inputElement, "t");
-     await new Promise((resolve) => setTimeout(resolve, 1000));
+  const inputElement = getByPlaceholderText("Search for a movie...");
 
-     const movieTitles = getAllByText(/t.*/i);
-   
-     movieTitles.forEach((titleElement) => {
-       expect(titleElement.textContent.toLowerCase()).toContain("t");
-     });
-   });
+  userEvent.type(inputElement, "t");
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const movieTitles = getAllByText(/t.*/i);
+
+  movieTitles.forEach((titleElement) => {
+    expect(titleElement.textContent.toLowerCase()).toContain("t");
+  });
+});
