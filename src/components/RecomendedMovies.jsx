41596e2e2e855b8c3b32546b6b5f11c6e16ae454
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-10-06 16:38:07 +0200

    installed and implemented carousel
    Installed react-alice-carousel and made a component with it for reusability. I added styling on the already existing css library from the package and thats why i had to use snake_case. Therefore i disabled stylelint selector-class-pattern in that css file.

--- src/components/RecomendedMovies.jsx
+++ src/components/RecomendedMovies.jsx
@@ -1,31 +1,28 @@
-import dataBase from '../data/movies.json';
-import MovieCard from './ui/MovieCard';
-import TitleLine from './ui/TitleLine';
+import dataBase from "../data/movies.json";
+import TitleLine from "./ui/TitleLine";
+import Carousel from "./Carousel";
 
 const RecomendedMovies = () => {
   // Plocka ut filmer som inte har trending=true
-  const nonTrendingMovies = dataBase.filter(movie => movie.isTrending !== true);
+  const nonTrendingMovies = dataBase.filter((movie) => !movie.isTrending);
 
   // Blanda de icke-trending filmerna slumpmässigt
-  function shuffleArray(array) {
+  const shuffleArray = (array) => {
     const shuffled = array.slice();
     for (let i = shuffled.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
     }
     return shuffled;
-  }
+  };
 
   // Välj fem slumpmässiga icke-trending filmer
-  const randomMovies = shuffleArray(nonTrendingMovies).slice(0, 5);
+  const randomMovies = shuffleArray(nonTrendingMovies).slice(0, 8);
+
   return (
     <div>
       <TitleLine text="Recommended for you" />
-      <div>
-        {randomMovies.map((movie) => (
-            <MovieCard key={movie.id} movie={movie} />
-        ))}
-      </div>
+      <Carousel data={randomMovies} />
     </div>
   );
 };
